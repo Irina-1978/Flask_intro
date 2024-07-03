3373c6e07c922d04820fba80e647ea042e4ff242
Irina-1978 <irina.nazarova@gmail.com>
2024-07-03 13:10:13 +0200

    Day 1

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,6 +24,12 @@ def create_app(test_config=None):
     def hello():
         return "Hello world"
     
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app 
 
 app = create_app()
